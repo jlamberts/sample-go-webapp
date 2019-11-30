9af25f8824e291c2a678e009f7251a560f685d62
Jonathan Lamberts <jonathan.lamberts@gmail.com>
2019-11-29 21:47:13 -0500

    created basic webserver

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
-func main() {
-	fmt.Println("Hello World")
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>welcome to my awesome site.</h1>")
+}
+
+func main()  {
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", nil)
 }
